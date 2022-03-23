81bf8c0a4c83396c756b9ba868dff2529d61b3cc
ziiaa <azzizahnur01@amikom.ac.id>
2022-03-23 22:27:45 +0700

    Taxi

---
+++ drivertaxi/taxi.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace drivertaxi
+{
+    public class taxi
+    {
+        public string DriverName { get; set; }
+        public string OnDuty { get; set; }
+        public string NumPassenger { get; set; }
+
+        public void Registrasi()
+        {
+            Console.WriteLine("Nama Driver : {0}", DriverName);
+            Console.WriteLine("Pick Up Passenger : {0}", OnDuty);
+            Console.WriteLine("Drop Off Passenger : {0}", NumPassenger);
+            Console.WriteLine("\nSedang Menjemput Penumpang");
+        }
+        public void Taxi()
+        {
+            Console.WriteLine("{0} Selesai Mengantar Penumpang\n", DriverName);
+        }
+    }
+}
+
+
+
